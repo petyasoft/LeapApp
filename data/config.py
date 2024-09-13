50ff24eaf05c2f4e5b95e9b5338e3582b1e76994
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-09-13 19:32:22 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,36 @@
+# api id, hash
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '283993:kdmioieiweikiokeocki4okew' # API TOKEN get in @BotFather
+CHAT_ID = '22803822' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [5, 15]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+
+REF_CODE = '6840081105'
+
+BLACKLIST_TASKS = {'Invite 5 friend','5 days in a row','Reach 2000 points','Reach 20 000 pt','10 days in a row','Reach 200 000 pt'}
+
+MINI_SLEEP = [2,8]
+
+BIG_SLEEP = [0*60*60,4*60*60]
+
+hello ='''              _                               __  _        
+ _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
+| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
+| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
+| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
+|_|                |___/        
+
+'''
